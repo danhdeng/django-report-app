8bc9b36efa30f9fd3a0764f24073a12ebc9fbe33
Dan Deng <hui_danny_deng@hotmail.com>
2021-08-06 13:23:23 -0400

    update

--- reports/views.py
+++ reports/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 from profiles.models import Profile
-# Create your views here.
+from .forms import ReportForm
 
+# Create your views here.
+@login_required
 def created_report_view(request):
-    print(request.is_ajax)
+    form=ReportForm(request.POST or None)
     if request.is_ajax:
-        name=request.POST.get('name')
-        remarks=request.POST.get('remarks')
-        image=request.POST.get('image')
-        name=request.POST.get('name')
+        # name=request.POST.get('name')
+        # remarks=request.POST.get('remarks')
+        # image=request.POST.get('image')
+        # name=request.POST.get('name')
 
         # author=Profile.objects.get(user=request.user)
+        if form.is_valid:
+            form.save()
         return JsonResponse({"message":"send"})
     return JsonResponse({"message":"not ajax call"})
 
